13d24d222de519e7747330823ab2c14b028f5e6c
Gerald Shen <gerald.shen@mail.utoronto.ca>
2021-07-06 15:59:27 -0400

    checkpointing

--- object_detector/train.py
+++ object_detector/train.py
@@ -27,34 +27,120 @@ from albumentations.pytorch import ToTensorV2
 from vector_cv_tools import datasets
 from vector_cv_tools import transforms as T
 from vector_cv_tools import utils
+from vector_cv_tools import checkpointing as vckpts
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", type=int, default=2000, help="number of epochs")
-parser.add_argument("--image_folder", type=str, default="data/artifacts/images", help="path to dataset")
-parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
-parser.add_argument("--model_config_path", type=str, default="object_detector/config/yolov3.cfg", help="path to model config file")
-parser.add_argument("--data_config_path", type=str, default="object_detector/config/coco.data", help="path to data config file")
-parser.add_argument("--weights_path", type=str, default="object_detector/config/yolov3.weights", help="path to weights file")
-parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
-parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
-parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
-parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-parser.add_argument("--checkpoint_interval", type=int, default=10, help="interval between saving model weights")
-parser.add_argument("--checkpoint_dir", type=str, default="checkpoints", help="directory where model checkpoints are saved")
-parser.add_argument("--use_cuda", type=bool, default=True, help="whether to use cuda if available")
+parser.add_argument("--image_folder",
+                    type=str,
+                    default="data/artifacts/images",
+                    help="path to dataset")
+parser.add_argument("--batch_size",
+                    type=int,
+                    default=16,
+                    help="size of each image batch")
+parser.add_argument("--model_config_path",
+                    type=str,
+                    default="object_detector/config/yolov3.cfg",
+                    help="path to model config file")
+parser.add_argument("--data_config_path",
+                    type=str,
+                    default="object_detector/config/coco.data",
+                    help="path to data config file")
+parser.add_argument("--weights_path",
+                    type=str,
+                    default="object_detector/config/yolov3.weights",
+                    help="path to weights file")
+parser.add_argument("--conf_thres",
+                    type=float,
+                    default=0.8,
+                    help="object confidence threshold")
+parser.add_argument("--nms_thres",
+                    type=float,
+                    default=0.4,
+                    help="iou thresshold for non-maximum suppression")
+parser.add_argument("--n_cpu",
+                    type=int,
+                    default=0,
+                    help="number of cpu threads to use during batch generation")
+parser.add_argument("--img_size",
+                    type=int,
+                    default=416,
+                    help="size of each image dimension")
+parser.add_argument("--checkpoint_interval",
+                    type=int,
+                    default=10,
+                    help="interval between saving model weights")
+parser.add_argument("--checkpoint_dir",
+                    type=str,
+                    default="checkpoints",
+                    help="directory where model checkpoints are saved")
+parser.add_argument("--use_cuda",
+                    type=bool,
+                    default=True,
+                    help="whether to use cuda if available")
 parser.add_argument('--eval', action='store_true')
 parser.add_argument('-thb', '--test_hardcoded_boxes', action='store_true')
-parser.add_argument(
-        '--data_dir',
-        type=str,
-        default="/scratch/ssd002/datasets/cv_project/forms_dataset")
+parser.add_argument('--data_dir',
+                    type=str,
+                    default="/scratch/ssd002/datasets/cv_project/forms_dataset")
 parser.add_argument('--data_split', type=str, default="train")
+
+# use this to differentiate between the different runs, otherwise
+# every run will be saved in the same directory
+parser.add_argument('--run_name', type=str, required=True)
+
 opt = parser.parse_args()
+
+
+def create_or_restore_training_state(model,
+                                     optimizer,
+                                     dir_path="./checkpoints/baseline_model"):
+    cur_epoch = vckpts.SavebleNumber(0)
+    prev_epoch_loss_list = vckpts.SaveableList()
+
+    checkpoint = vckpts.SavableCollection(
+        model=model,
+        optimizer=optimizer,
+        cur_epoch=cur_epoch,
+        prev_epoch_loss_list=prev_epoch_loss_list,
+    )
+
+    manager = vckpts.CheckpointManager(
+        checkpoint=checkpoint,
+        directory=dir_path,
+        max_to_keep=3,
+        checkpoint_interval=1200,
+        keep_best="max",
+    )
+
+    # this call does nothing if there is no checkpoint loaded
+    # otherwise it loads from the latest checkpoint in the directory
+    # specified above
+    manager.load_latest_checkpoint()
+
+    if manager.latest_checkpoint is not None:
+        min_loss = (checkpoint.prev_epoch_loss_list[-1]
+                    if len(checkpoint.prev_epoch_loss_list) > 0 else None)
+        print_str = (
+            f"Checkpoint that has finished epoch {checkpoint.cur_epoch} with "
+            f"val loss {min_loss} is loaded from {manager.latest_checkpoint}")
+
+    else:
+        print_str = (f"No checkpoints found under {manager.directory}, "
+                     "starting from scratch")
+
+    print(print_str)
+
+    cur_epoch.add_(1)
+    return cur_epoch, model, optimizer, manager, prev_epoch_loss_list
+
 
 def train():
     cuda = torch.cuda.is_available() and opt.use_cuda
 
-    os.makedirs("checkpoints", exist_ok=True)
+    checkpoint_dir = os.path.join("./checkpoints", opt.run_name)
+    os.makedirs(checkpoint_dir, exist_ok=True)
 
     # Get data configuration
     data_config = parse_data_config(opt.data_config_path)
@@ -69,17 +155,19 @@ def train():
 
     # Initiate model
     model = Darknet(opt.model_config_path)
-    # model.load_weights(opt.weights_path)
     model.apply(weights_init_normal)
+
+    optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()))
+
     if cuda:
         model = model.cuda()
 
-    model.train()
+    # load either non initialized or loaded state
+    cur_epoch, model, optimizer, manager, prev_epoch_loss_list = create_or_restore_training_state(
+        model, optimizer, dir_path=checkpoint_dir)
 
-    # Get dataloader
-    # dataloader = torch.utils.data.DataLoader(
-    #     ListDataset(train_path), batch_size=opt.batch_size, shuffle=False, num_workers=opt.n_cpu
-    # )
+    model.train()
 
     transform = [ToTensorV2()]
     transform = T.ComposeFUNSDTransform(transform, img_size=opt.img_size)
@@ -89,6 +177,7 @@ def train():
                           load_qa_only=True,
                           load_linked_only=True,
                           linking_limit=1)
+
     dataloader = DataLoader(dset,
                             num_workers=opt.n_cpu,
                             batch_size=opt.batch_size,
@@ -96,10 +185,10 @@ def train():
 
     Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 
-    optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()))
+    while cur_epoch < opt.epochs + 1:
 
-    for epoch in range(opt.epochs):
-        # for batch_i, (_, imgs, targets) in enumerate(dataloader):
+        epoch_loss_list = []
+
         for batch_i, (imgs, targets) in enumerate(dataloader):
             imgs = Variable(imgs.type(Tensor))
             targets = Variable(targets.type(Tensor), requires_grad=False)
@@ -113,7 +202,7 @@ def train():
             print(
                 "[Epoch %d/%d, Batch %d/%d] [Losses: x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, recall: %.5f, precision: %.5f]"
                 % (
-                    epoch,
+                    cur_epoch,
                     opt.epochs,
                     batch_i,
                     len(dataloader),
@@ -126,12 +215,13 @@ def train():
                     loss.item(),
                     model.losses["recall"],
                     model.losses["precision"],
-                )
-            )
+                ))
 
             model.seen += imgs.size(0)
 
-        if epoch % opt.checkpoint_interval == 0:
+            epoch_loss_list.append(loss.item())
+
+        if cur_epoch % opt.checkpoint_interval == 0:
             img_path = 'object_detector/images/82092117.png'
             img = Image.open(img_path)
             img = np.array(img)
@@ -139,25 +229,41 @@ def train():
                 img = img[:, :, np.newaxis]
                 img = np.repeat(img, 3, axis=2)
                 img = Image.fromarray(img)
-            model.save_weights("%s/%d.weights" % (opt.checkpoint_dir, epoch))
+            # model.save_weights("%s/%d.weights" % (opt.checkpoint_dir, epoch))
             detections = detect_image(img, opt.img_size, model)
-            plot_box(img, opt.img_size, detections, (1, 0, 0), img_path.replace(".jpg", "_{}.jpg".format(epoch)).replace(".png", "_{}.png".format(epoch)), "locations")
-            
+            plot_box(
+                img, opt.img_size, detections, (1, 0, 0),
+                img_path.replace(".jpg", "_{}.jpg".format(cur_epoch)).replace(
+                    ".png", "_{}.png".format(cur_epoch)), "locations")
+
+        epoch_loss = 0 if len(epoch_loss_list) == 0 else sum(
+            epoch_loss_list) / len(epoch_loss_list)
+        print("Loss over epoch {} is {}".format(cur_epoch, epoch_loss))
+
+        prev_epoch_loss_list.append(epoch_loss)
+        manager.save(global_step=cur_epoch, metric=epoch_loss, do_logging=True)
+
+        cur_epoch.add_(1)
+
 
 def detect_image(img, img_size, model):
     # scale and pad image
-    conf_thres=0.8
-    nms_thres=0.4
+    conf_thres = 0.8
+    nms_thres = 0.4
     Tensor = torch.cuda.FloatTensor
-    ratio = min(img_size/img.size[0], img_size/img.size[1])
+    ratio = min(img_size / img.size[0], img_size / img.size[1])
     imw = round(img.size[0] * ratio)
     imh = round(img.size[1] * ratio)
 
-    img_transforms = transforms.Compose([ transforms.Resize((imh, imw)),
-         transforms.Pad((max(int((imh-imw)/2),0), max(int((imw-imh)/2),0), max(int((imh-imw)/2),0), max(int((imw-imh)/2),0)),
-                        (128,128,128)),
-         transforms.ToTensor(),
-         ])
+    img_transforms = transforms.Compose([
+        transforms.Resize((imh, imw)),
+        transforms.Pad((max(int(
+            (imh - imw) / 2), 0), max(int(
+                (imw - imh) / 2), 0), max(int(
+                    (imh - imw) / 2), 0), max(int((imw - imh) / 2), 0)),
+                       (128, 128, 128)),
+        transforms.ToTensor(),
+    ])
     # convert image to Tensor
     # print(img.shape)
     image_tensor = img_transforms(img).float()
@@ -169,53 +275,74 @@ def detect_image(img, img_size, model):
         detections = non_max_suppression(detections, 80, conf_thres, nms_thres)
     return detections[0]
 
+
 def test_hardcoded_boxes():
-    img_size=opt.img_size
-    img_path = "images/82092117.png" # form
+    img_size = opt.img_size
+    img_path = "images/82092117.png"  # form
     # img_path = 'images/000012.jpg' # car
     prev_time = time.time()
     img = Image.open(img_path)
-    
+
     # detections = torch.tensor([[116, 352, 27, 15, 0.9, 0.9, 4]]) # form field 1
-    detections = torch.tensor([[188,  512, 171, 17, 0.9, 0.9, 4]]) # form field 2
+    detections = torch.tensor([[188, 512, 171, 17, 0.9, 0.9,
+                                4]])  # form field 2
     # detections = torch.tensor([[253.5, 183.499999999983, 195.0, 173.00000000016, 0.9, 0.9, 4]]) # car
 
     cmap = plt.get_cmap('tab20b')
     colors = [cmap(i) for i in np.linspace(0, 1, 20)]
 
-    plot_box(img, img_size, detections, (1, 0, 0), img_path.replace(".jpg", "-det.jpg").replace(".png", "-det.png"), "width_height")
+    plot_box(img, img_size, detections, (1, 0, 0),
+             img_path.replace(".jpg", "-det.jpg").replace(".png", "-det.png"),
+             "width_height")
+
 
 def evaluation():
-    img_size=opt.img_size
+    img_size = opt.img_size
 
     # Load model and weights
     model = Darknet(opt.model_config_path, img_size=img_size)
-    model.load_weights(opt.weights_path)
+
+    ############################## NOTE ##########################
+
+    # instead of doing this:
+    # model.load_weights(opt.weights_path)
+
+    # do this instead
+    model.load_state_dict(torch.load(opt.weights_path)["model"])
+
     model.cuda()
     model.eval()
 
     # load image and get detections
-    img_path = "images/82092117.png" # form
+    img_path = "images/82092117.png"  # form
     # img_path = 'images/000012.jpg' # car
     prev_time = time.time()
     img = Image.open(img_path)
     detections = detect_image(img, img_size, model)
-    
+
     # detections = torch.tensor([[0.5070, 0.5330, 0.3900, 0.3460, 0.9, 0.9, 3]])
     # detections = torch.tensor([[10.3952, 23.7931,  1.0586,  1.0345, 0.9, 0.9, 4]])
     inference_time = datetime.timedelta(seconds=time.time() - prev_time)
-    print ('Inference Time: %s' % (inference_time))
+    print('Inference Time: %s' % (inference_time))
 
     # Get bounding-box colors
     cmap = plt.get_cmap('tab20b')
     colors = [cmap(i) for i in np.linspace(0, 1, 20)]
 
-    plot_box(img, img_size, detections, (1, 0, 0), img_path.replace(".jpg", "-det.jpg").replace(".png", "-det.png"), "width_height")
+    plot_box(img, img_size, detections, (1, 0, 0),
+             img_path.replace(".jpg", "-det.jpg").replace(".png", "-det.png"),
+             "width_height")
 
-def plot_box(img, img_size, detections, color, img_path, prediction_type='width_height'):
+
+def plot_box(img,
+             img_size,
+             detections,
+             color,
+             img_path,
+             prediction_type='width_height'):
     img = np.array(img)
     plt.figure()
-    fig, ax = plt.subplots(1, figsize=(12,9))
+    fig, ax = plt.subplots(1, figsize=(12, 9))
     ax.imshow(img)
 
     pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
@@ -231,7 +358,12 @@ def plot_box(img, img_size, detections, color, img_path, prediction_type='width_
         for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
             # print(x1, y1, x2, y2)
             if prediction_type == 'width_height':
-                bbox = patches.Rectangle((x1, y1), x2, y2, linewidth=2, edgecolor=color, facecolor='none')
+                bbox = patches.Rectangle((x1, y1),
+                                         x2,
+                                         y2,
+                                         linewidth=2,
+                                         edgecolor=color,
+                                         facecolor='none')
             else:
                 box_h = ((y2 - y1) / unpad_h) * img.shape[0]
                 box_w = ((x2 - x1) / unpad_w) * img.shape[1]
@@ -240,10 +372,22 @@ def plot_box(img, img_size, detections, color, img_path, prediction_type='width_
                 # print(x1, y1, x2, y2)
                 # color = bbox_colors[int(np.where(unique_labels == int(cls_pred))[0])]
                 # print(x1, y1, box_w, box_h)
-                bbox = patches.Rectangle((x1, y1), box_w, box_h, linewidth=2, edgecolor=color, facecolor='none')
+                bbox = patches.Rectangle((x1, y1),
+                                         box_w,
+                                         box_h,
+                                         linewidth=2,
+                                         edgecolor=color,
+                                         facecolor='none')
             ax.add_patch(bbox)
-            plt.text(x1, y1, s='.', color='white', verticalalignment='top',
-                    bbox={'color': color, 'pad': 0})
+            plt.text(x1,
+                     y1,
+                     s='.',
+                     color='white',
+                     verticalalignment='top',
+                     bbox={
+                         'color': color,
+                         'pad': 0
+                     })
     plt.axis('off')
     # save image
     plt.savefig(img_path, bbox_inches='tight', pad_inches=0.0)
